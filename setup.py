92232299cfd1a9f3f16cd0b12bd67982edae080b
Zachary Voase <z@zacharyvoase.com>
2011-04-19 15:27:23 +0000

    Updated setup.py and _get_tests() for new package layout.

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ from distribute_setup import use_setuptools
 use_setuptools()
 
 import re
-from setuptools import setup
+from setuptools import setup, find_packages
 import os.path as p
 
 
 def get_version():
-    source = open(p.join(p.dirname(p.abspath(__file__)), 'jsonpipe.py')).read()
+    source = open(p.join(p.dirname(p.abspath(__file__)),
+                         'src', 'jsonpipe', '__init__.py')).read()
     match = re.search(r'__version__\s*=\s*[\'"]([^\'"]+)[\'"]', source)
     if not match:
-        raise RuntimeError("Couldn't find the version string in jsonpipe.py")
+        raise RuntimeError("Couldn't find the version string in src/jsonpipe/__init__.py")
     return match.group(1)
 
 
@@ -24,9 +25,10 @@ setup(
     author='Zachary Voase',
     author_email='z@dvxhouse.com',
     url='http://github.com/dvxhouse/jsonpipe',
-    py_modules=['jsonpipe'],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     entry_points={'console_scripts': ['jsonpipe = jsonpipe:main',
                                       'jsonunpipe = jsonpipe:main_unpipe']},
-    install_requires=['simplejson>=2.1.3', 'argparse>=1.2.1'],
+    install_requires=['simplejson>=2.1.3', 'argparse>=1.2.1', 'calabash>=0.0.2'],
     test_suite='jsonpipe._get_tests',
 )
--- src/jsonpipe/__init__.py
+++ src/jsonpipe/__init__.py
@@ -14,8 +14,43 @@ __version__ = '0.0.5'
 
 def _get_tests():
     import doctest
-    return doctest.DocTestSuite(optionflags=(doctest.ELLIPSIS |
-                                             doctest.NORMALIZE_WHITESPACE))
+    import inspect
+    import sys
+    import unittest
+
+    import jsonpipe.sh
+
+    def _from_module(module, object):
+        """Backported fix for http://bugs.python.org/issue1108."""
+        if module is None:
+            return True
+        elif inspect.getmodule(object) is not None:
+            return module is inspect.getmodule(object)
+        elif inspect.isfunction(object):
+            return module.__dict__ is object.func_globals
+        elif inspect.isclass(object):
+            return module.__name__ == object.__module__
+        elif hasattr(object, '__module__'):
+            return module.__name__ == object.__module__
+        elif isinstance(object, property):
+            return True # [XX] no way not be sure.
+        else:
+            raise ValueError("object must be a class or function")
+    finder = doctest.DocTestFinder()
+    finder._from_module = _from_module
+
+    suite = unittest.TestSuite()
+    for name, module in sys.modules.iteritems():
+        if name.startswith('jsonpipe'):
+            try:
+                mod_suite = doctest.DocTestSuite(
+                    module, test_finder=finder,
+                    optionflags=(doctest.NORMALIZE_WHITESPACE |
+                                 doctest.ELLIPSIS))
+            except ValueError:
+                continue
+            suite.addTests(mod_suite)
+    return suite
 
 
 PARSER = argparse.ArgumentParser()
